9794c6c4b2cd68a7f7f9f4443e502f0851978d23
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-16 14:38:43 +0300

    This commit injects the http

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
 
+import 'rxjs/add/operator/map';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string;
+  private token= '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';
 
   constructor() { }
 }
