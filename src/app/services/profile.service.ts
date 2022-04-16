e39805add6c4f9f8e9ee4181342878a892e0f889
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-16 19:07:35 +0300

    This commit writes calls the api

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers} from '@angular/http';
+import{HttpClient } from  '@angular/common/http';
 
-import 'rxjs/add/operator/map';
-@Injectable({
-  providedIn: 'root'
-})
+import { map } from 'rxjs/internal/operators/map';
+
+
+@Injectable()
+
 export class ProfileService {
+
   private username:string;
+ 
   private token= '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';
 
-  constructor() { }
+  constructor( private http:HttpClient) { 
+    console.log("service is now ready!");
+    this.username = 'Dianakariuki';
+
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/").pipe(map((_data: any) => {})) + this.username + "&token=" + this.token;
+
+    
+  }
 }
